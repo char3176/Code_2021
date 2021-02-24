214a343fbf7d02abff6df672a77a613834fa473a
Sam Cordry <smartnerdguy13@gmail.com>
2021-02-23 20:52:42 -0500

    Take Vision off of master branch

--- Vision/src/main/java/frc/robot/Constants.java
+++
@@ -1,7 +0,0 @@
-package frc.robot;
-
-public final class Constants {
-    public static final double VISION_CONSTANT = 0; // CHANGE BEFORE RUNNING
-    public static final double DELTA_X_REAL = 39.25 / 12;
-    public static final double DEG2RAD = Math.PI / 180;
-}
