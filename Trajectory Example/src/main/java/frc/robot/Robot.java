656bebfa44dc838fcb4e6ff9ed5cd160007478d7
NebulaVGC <cwtdawg@gmail.com>
2021-02-27 10:15:11 -0500

    Created more paths

--- Trajectory Example/src/main/java/frc/robot/Robot.java
+++ Trajectory Example/src/main/java/frc/robot/Robot.java
@@ -43,7 +43,7 @@ public class Robot extends TimedRobot {
   public void robotInit() {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
-    String trajectoryJSON = "paths/Help.wpilib.json";
+    String trajectoryJSON = "paths/Complicated.wpilib.json";
     Trajectory trajectory = new Trajectory();
     try {
       Path trajectoryPath = Filesystem.getDeployDirectory().toPath().resolve(trajectoryJSON);
