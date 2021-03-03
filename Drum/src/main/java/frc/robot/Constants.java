c78f93f15a3504735ed85f95816494714acbc5d8
JaredTheBro <jaredab370@gmail.com>
2021-03-02 20:50:45 -0500

    My Drum speeds and ramp were SLOW. Now they're (kind of) FAST.

--- Drum/src/main/java/frc/robot/Constants.java
+++ Drum/src/main/java/frc/robot/Constants.java
@@ -18,29 +18,21 @@ public final class Constants {
 
     public static final int drumMotorCANID = 6;
 
-    /* // Drum PIDF constants for SparkMAX BIG Neo (number?)
-    public static final double drumKP = 0.000121;
-    public static final double drumKI = 0.0000024;
-    public static final double drumKD = 0.00272;
-    public static final double drumKF = 0.000169;
-    public static final double drumKIZone = 48;
-    public static final double drumKRampRate = 4500;    */
-
-    // Drum PIDF Constants for SparkMAX Neo 550 - NEEDS A LITTLE TUNING/CHECKING
+    // Drum PIDF Constants for SparkMAX Neo 550
     public static final double drumKP = 0.00013;
     public static final double drumKI = 0.0000008;
     public static final double drumKD = 0.003;
     public static final double drumKF = 0.000097;
-    public static final double drumKIZone = 0; // SET THIS TO SOMETHING?
-    public static final double drumKRampRate = 2500;    
+    public static final double drumKIZone = 0; // does this really need to be messed with?
+    public static final double drumKRampRate = 4000;    
 
     public static final double drumKMaxOutput = 1.0;
     public static final double drumKMinOutput = -1.0;
 
     // RPM for Drum buttons
-    public static final int drumLow = 1500;
-    public static final int drumMedium = 3500;
-    public static final int drumHigh = 6000;
+    public static final int drumLow = 2500;
+    public static final int drumMedium = 4500;
+    public static final int drumHigh = 7000;
     public static final int drumExtreme = 8500;
 
 }
