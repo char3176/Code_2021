b638f36194a4aec1bdcefa7a7cf9bdcce84357fe
Robotics-SL07 <team3176@gmail.com>
2021-03-11 19:23:02 -0500

    wrote code that set modes for the camera

--- LeftOrRight/src/main/java/frc/robot/VisionClient.java
+++ LeftOrRight/src/main/java/frc/robot/VisionClient.java
@@ -21,6 +21,40 @@ public class VisionClient {
         limelightTable = tableInstance.getTable("limelight");
         tv = limelightTable.getEntry("tv");
         tx = limelightTable.getEntry("tx");
-        limelightTable.getEntry("pipeline").setNumber(3);
+        limelightTable.getEntry("pipeline").setNumber(0);
+    }
+    public void turnLEDson(){
+        limelightTable.getEntry("ledMode").setNumber(3);
+    }
+    public void blinkLEDs(){
+        limelightTable.getEntry("ledMode").setNumber(2);
+    }
+    public void turnLEDsoff(){
+        limelightTable.getEntry("ledMode").setNumber(1);
+    }
+    /**
+     * Sets camera's mode
+     * @param mode determines which mode the method is set to, 0 is vision processing, 1 is Driver Cam, and anything else will print an error message.
+     */
+    public void setCameraMode(boolean mode){
+        if(mode){
+            limelightTable.getEntry("camMode").setNumber(0);
+        }
+        else{
+            limelightTable.getEntry("camMode").setNumber(1);
+        }
+    }
+    
+    /**
+     * Sets which pipeline the processor will use.
+     * @param pipe sets the pipeline that will be used.
+     */
+    public void setPipeline(int pipe){
+        if(pipe == 0){
+            limelightTable.getEntry("pipeline").setNumber(0);
+        }
+        else if(pipe == 1){
+            limelightTable.getEntry("pipeline").setNumber(1);
+        }
     }
 }
