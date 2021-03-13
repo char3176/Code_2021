58c4d483654510f00a1955c255c227017024458b
DriveStation#1 <char3176@gmail.com>
2021-03-13 15:38:36 -0500

    Uncommented Intake

--- Akriveia/src/main/java/frc/robot/subsystems/Intake.java
+++ Akriveia/src/main/java/frc/robot/subsystems/Intake.java
@@ -1,16 +1,16 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-// import edu.wpi.first.wpilibj.DoubleSolenoid;
-// import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import frc.robot.constants.IntakeConstants;
 
 public class Intake extends SubsystemBase {
   private static Intake instance = new Intake();
-  // private DoubleSolenoid leftPiston = new DoubleSolenoid(2, 5);
-  // private DoubleSolenoid rightPiston = new DoubleSolenoid(1, 7);
+  private DoubleSolenoid leftPiston = new DoubleSolenoid(2, 5);
+  private DoubleSolenoid rightPiston = new DoubleSolenoid(1, 7);
   private WPI_TalonSRX motor = new WPI_TalonSRX(IntakeConstants.MOTOR_CAN_ID);
 
   public Intake() {
@@ -26,13 +26,13 @@ public class Intake extends SubsystemBase {
     motor.set(ControlMode.PercentOutput, percent);
   }
 
-  // public void Extend() {
-  //   leftPiston.set(Value.kForward);
-  //   rightPiston.set(Value.kForward);
-  // }
+  public void Extend() {
+    leftPiston.set(Value.kForward);
+    rightPiston.set(Value.kForward);
+  }
 
-  // public void Retract() {
-  //   leftPiston.set(Value.kReverse);
-  //   rightPiston.set(Value.kReverse);
-  // }
+  public void Retract() {
+    leftPiston.set(Value.kReverse);
+    rightPiston.set(Value.kReverse);
+  }
 }
