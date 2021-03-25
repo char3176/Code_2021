27042bcd2cc99354287393e174759463ea4a5159
char@odin <char@odin>
2021-03-25 14:46:07 -0400

    addition of ElectricalData data structure class for use with PowerManagement.java.  Converted PowerManagement into subsystem to take advantage of asynchronized periodic function.

---
+++ Akriveia/src/main/java/frc/robot/constants/PowerManagementConstants.java
@@ -0,0 +1,8 @@
+package frc.robot.constants;
+
+public class PowerManagementConstants {
+    public static final int PDP_CAN_ID = 0;
+    public static final int DRUM_PDP_CHANNEL = 1;
+    public static final int ANGLED_SHOOTER_PDP_CHANNEL = 8;
+    
+}
---
+++ Akriveia/src/main/java/frc/robot/subsystems/PowerManagement.java
@@ -0,0 +1,55 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.PowerDistributionPanel;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.constants.PowerManagementConstants;
+import frc.robot.util.ElectricalData;
+
+public class PowerManagement extends SubsystemBase {
+    private PowerDistributionPanel PDP; 
+    private static PowerManagement instance = new PowerManagement();
+
+    /* We assume an update rate of 1 update per 0.02 sec; therefore, we collect 5 values in the Current History,
+     * which represents a window of 0.1 sec wide (10% of a second).
+     * Consider expanding to 15 values which would represent a 0.25sec wide window. */
+   
+    private ElectricalData HoodPM; 
+    private ElectricalData DrumPM; 
+   
+    public PowerManagement() {
+        PowerDistributionPanel PDP = new PowerDistributionPanel(PowerManagementConstants.PDP_CAN_ID);
+        ElectricalData HoodPM = new ElectricalData(PowerManagementConstants.ANGLED_SHOOTER_PDP_CHANNEL, "Angled Shooter", 5);
+        ElectricalData DrumPM = new ElectricalData(PowerManagementConstants.DRUM_PDP_CHANNEL, "Drum", 5);
+    }
+
+    private double getInstantaneousAmp(int pdpChannel) {
+        return PDP.getCurrent(pdpChannel);
+    }
+    
+    public double getAngledShooterInstantAmp() {
+        return HoodPM.getInstantaneousAmp();
+    }
+
+    public double getAngledShooterAvgAmp() {
+        return HoodPM.mean();
+    }
+
+    public double getDrumInstantAmp() {
+        return DrumPM.getInstantaneousAmp();
+    }
+
+    public double getDrumAvgAmp() {
+        return DrumPM.mean();
+    }
+
+    @Override
+    public void periodic() {
+        HoodPM.addAmpData();
+        DrumPM.addAmpData();
+    }
+
+    public static PowerManagement getInstance() {
+        return instance; 
+    }
+
+}
