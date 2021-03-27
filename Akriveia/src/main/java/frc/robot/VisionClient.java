ff53889a497b37aa61567b6e1e58b225b21a7ffd
Jonathan Heidegger <jth3141@gmail.com>
2021-03-27 10:44:30 -0400

    Fixed some incorrect syntax in VisionClient.java

--- Akriveia/src/main/java/frc/robot/VisionClient.java
+++ Akriveia/src/main/java/frc/robot/VisionClient.java
@@ -108,7 +108,7 @@ public class VisionClient{
      * -initial velocity of ball to make it into target
      * @return if the method has found a valid solution
      */
-    public double[] targetRecogControlLoop(){
+    public boolean targetRecogControlLoop(){
         // used to calculate latency
         double startTime = Timer.getFPGATimestamp();
 
