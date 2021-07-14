7e9ce69e8f867f7d7eb159d1572aa5464b85538e
Kyle <45471789+kphillips2003@users.noreply.github.com>
2021-07-13 23:34:06 -0400

    Updated IR Counter b/c forgot to add lastState boolean to not rack up multiple on a ball

--- Akriveia/src/main/java/frc/robot/commands/auton/IrCounter.java
+++ Akriveia/src/main/java/frc/robot/commands/auton/IrCounter.java
@@ -10,6 +10,7 @@ import frc.robot.subsystems.Transfer;
 public class IrCounter extends CommandBase {
   Transfer m_Transfer = Transfer.getInstance();
   int count;
+  boolean lastState;
 
   public IrCounter() {
     addRequirements(m_Transfer);
@@ -24,10 +25,12 @@ public class IrCounter extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if(m_Transfer.getIrSensor()) {
+    if(m_Transfer.getIrSensor() && lastState == false) {
       count++;
+      lastState = true;
       System.out.println("----IR__COUNT----");
     }
+    else {lastState = false;}
   }
 
   // Called once the command ends or is interrupted.
